cd5c8ebbb23657acd804c534e13725d6d51a1371
vy vo <tuonggvy.vo@gmail.com>
2019-11-03 04:15:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   reverse_sentence = []
   sentence_array = sentence.split(" ")
   sentence_array.collect do |word|
-    word.reverse
+    reverse_sentence << word.reverse
   end
+  reverse_sentence.join(" ")
 end
