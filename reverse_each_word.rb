db199ff9a5663ac15fcd013b3a0353cddc02965d
vy vo <tuonggvy.vo@gmail.com>
2019-11-03 04:13:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,5 @@ def reverse_each_word(sentence)
   reverse_sentence = []
   sentence_array = sentence.split(" ")
   sentence_array.collect do |word|
-    reverse_sentence
-  end
-  reverse_sentence.join(" ")
+    reverse_sentenc
 end
